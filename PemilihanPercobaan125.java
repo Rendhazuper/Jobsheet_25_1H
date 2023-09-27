8f2a9d6cba3c0389671ccab8bb0ea81ac731bef1
Rendha <rendhazuper@gmail.com>
2023-09-27 07:55:03 +0700

    Praktikum 1 Percobaan 5

--- PemilihanPercobaan125.java
+++ PemilihanPercobaan125.java
@@ -1,18 +1,15 @@
-package Jobsheet_25_1H;
 import java.util.Scanner; 
 public class PemilihanPercobaan125 {
     
     public static void main(String[] args) {
         
         Scanner input25 = new Scanner (System.in);
+        String hasil;
 
         System.out.println("Masukkan angka : ");
         int angka = input25.nextInt();
 
-        if (angka %2 == 0){
-            System.out.println("Angka "+angka+" Bilangan genap");
-        } else {
-            System.out.println("Angka "+angka+" Bilangan ganjil");
-        }
+        hasil = (angka %2 == 0) ?  "Bilangan Genap" : "Bilangan negatif";
+        System.out.println(angka + " adalah " + hasil);
     }
 }
